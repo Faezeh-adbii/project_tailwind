0b320c53de51f9c10f10df5a21bd6ab843eba9d4
Faezeh <faezehadabi9@gmail.com>
2025-10-14 17:53:24 +0330

    Updated config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
  content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens:{
+
+      sm:'570px',
+      md:'768px',
+      lg:'992px',
+      xl:'1200px',
+
+    },
+    container:{
+      center:true
+    },
+    extend: {
+      fontFamily:{
+        roboto:'Open Sans ,  sans-serif'
+      }
+    },
   },
   plugins: [],
 }
