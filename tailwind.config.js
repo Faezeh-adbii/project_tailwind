63db2d3c61e945efc6011cb347738de411f9cf2d
Faezeh <faezehadabi9@gmail.com>
2025-10-18 13:26:36 +0330

    Build 2a in the slider

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
- content: ["./public/**/*.{html,js}"],
+  content: ["./public/**/*.{html,js}"],
   theme: {
-    screens:{
+    screens: {
 
-      sm:'570px',
-      md:'768px',
-      lg:'992px',
-      xl:'1200px',
+      sm: '570px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
 
     },
-    container:{
-      center:true
+    container: {
+      center: true
     },
     extend: {
-      fontFamily:{
-        roboto:'Open Sans ,  sans-serif'
+      fontFamily: {
+        roboto: 'Open Sans ,  sans-serif'
       },
-      colors:{
-        
-        'graye-violet-20':'#fbf8fd',
-        'graye-violet-50':'#bbb9bb',
-        'gray-violet-200':'#9f9ca0',
-        'gray-violet-500':'#616061',
-        'violet-500':'#7c19a1',
-        'gray-red-50':'#998080',
+      colors: {
+
+        'graye-violet-20': '#fbf8fd',
+        'graye-violet-50': '#bbb9bb',
+        'gray-violet-200': '#9f9ca0',
+        'gray-violet-500': '#616061',
+        'violet-500': '#7c19a1',
+        'gray-red-50': '#998080',
+        'gray-red-100': '#5f5757',
       },
-      fontSize:{
-        xss:'10px',
+      fontSize: {
+        xss: '10px',
       },
-      spacing:{
-        6.25:'25px',
+      spacing: {
+        6.25: '25px',
+        15: '60px',
+        42: '170px',
         640: '40rem',
+
       },
+      flex:{
+        2:'2 2 0',
+      }
     },
   },
   plugins: [],
