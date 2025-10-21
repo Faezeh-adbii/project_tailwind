6639e7ac8e7086dd6f35f105ecb25dcdcb634d91
Faezeh <faezehadabi9@gmail.com>
2025-10-21 09:52:34 +0330

    build blog section

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,13 +24,17 @@ module.exports = {
         'gray-violet-200': '#9f9ca0',
         'gray-violet-500': '#616061',
         'gray-violet-600': '#39333c',
+        'gray-violet-700': '#3c3840',
         'violet-500': '#7c19a1',
+        'gray-red-30': '#958b8b',
         'gray-red-50': '#998080',
+        'gray-red-70': '#bcb2b2',
         'gray-red-100': '#5f5757',
         'purple-red': '#990e35',
       },
       fontSize: {
         xss: '10px',
+        'xs-medum':'13px',
       },
       spacing: {
         6.25: '25px',
@@ -46,6 +50,7 @@ module.exports = {
       gridTemplateColumns: {
         "new-arrival": 'repeat(auto-fit,minmax(260px,1fr))',
         "feature-product": 'repeat(auto-fit,minmax(270px,1fr))',
+        "blog": 'repeat(auto-fit,minmax(360px,1fr))',
       },
       backgroundImage: {
         commode: "url('../src/images/commode.png')"
